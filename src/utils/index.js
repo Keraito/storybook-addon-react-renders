e6880afce29737764063643f6d9d050cb52d5d01
Keraito <dragoon_v2_chak_shun@hotmail.com>
2018-06-26 20:02:14 +0200

    Improve punctuation

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,11 +8,11 @@ export const DIFF_TYPES = {
 export const formatTitle = ({ name, type }) => {
   switch (type) {
     case DIFF_TYPES.SAME:
-      return `${name}: Value is the same (equal by reference). Avoidable re-render!`;
+      return `${name}: Value is the same (equal by reference). Avoidable re-render`;
     case DIFF_TYPES.EQUAL:
-      return `${name}: Value did not change, avoidable re-render!`;
+      return `${name}: Value did not change, avoidable re-render`;
     case DIFF_TYPES.FUNCTIONS:
-      return `${name}: Changes are in functions only. Possibly avoidable re-render?`;
+      return `${name}: Changes are in functions only. Possibly avoidable re-render`;
     default:
       return name;
   }
