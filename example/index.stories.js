db950f68f5dab885a593f023b3106b7ceee4a425
Keraito <dragoon_v2_chak_shun@hotmail.com>
2018-07-08 23:30:33 +0200

    Cleanup the stories

--- example/index.stories.js
+++ example/index.stories.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
-import { withLifeCycle } from "../src/";
-import { Button, Welcome } from "@storybook/react/demo";
+import { Button } from "@storybook/react/demo";
 
 class DemoComponent extends React.Component {
   constructor(props) {
@@ -44,22 +41,6 @@ class DemoComponent extends React.Component {
     );
   }
 }
-
-// storiesOf("Welcome", module).add("to Storybook", () => (
-//   <Welcome showApp={linkTo("Button")} />
-// ));
-
-// storiesOf("Button", module)
-//   .add("with text", () => (
-//     <Button onClick={action("clicked")}>Hello Button</Button>
-//   ))
-//   .add("with some emoji", () => (
-//     <Button onClick={action("clicked")}>
-//       <span role="img" aria-label="so cool">
-//         😀 😎 👍 💯
-//       </span>
-//     </Button>
-//   ));
 
 storiesOf("Lifecycle addon", module)
   .add("Single component events", () => <DemoComponent />)
